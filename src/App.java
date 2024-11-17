0b0ba9d0753d9e9402c1cefdd367c5fdeddab874
Jari Antero Hiitola <jari.hiitola@student.hamk.fi>
2024-11-17 23:48:19 +0200

    Vaihdettu koodinpätkä

--- src/App.java
+++ src/App.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        // Testiksi joku ensimmäinen lisäys
-
+        
+        Scanner in = new Scanner(System.in);
+        String input = "";
+        String [] apho = {
+            "Actions speak louder than words.",
+            "A barking dog never bites.",
+            "A penny saved is a penny earned.",
+            "All things come to those who wait." };
+               
+        System.out.println("Pick number from 1-4.");
+        input = in.nextLine();
+        System.out.println(apho[Integer.parseInt(input)-1]);              
     }
+    // "Uusittu" koodinpätkä 17.11. Jari H.
 }
+
